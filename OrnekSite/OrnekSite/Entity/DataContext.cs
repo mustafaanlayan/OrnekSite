b3469920d3d0901ec4a9c4504f381f580e35cf40
mustafaanlayan <32699486+mustafaanlayan@users.noreply.github.com>
2021-12-19 01:25:20 +0300

    commit database

---
+++ OrnekSite/OrnekSite/Entity/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OrnekSite.Entity
+{
+    public class DataContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ OrnekSite/OrnekSite/Entity/DataInitilazer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OrnekSite.Entity
+{
+    public class DataInitilazer:DropCreateDatabaseIfModelChanges<DataContext>
+    {
+        protected override void Seed(DataContext context)
+        {
+            var kategoriler = new List<Category>()
+            {
+                new Category(){Name="KAMERA",Description="KAMERA ÜRÜNLERİ"},
+                new Category(){Name="TELEFON",Description="TELEFON ÜRÜNLERİ"},
+                new Category(){Name="BİLGİSAYAR",Description="BİLGİSAYAR ÜRÜNLERİ"}
+            };
+            foreach (var kategori in kategoriler)
+            {
+                context.Categories.Add(kategori);
+            }
+            context.SaveChanges();
+            base.Seed(context);
+        }
+    }
+}
